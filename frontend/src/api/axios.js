b343db807a626c307d0c64f27bc11fda1905ab7a
NatyraK363 <nk67363@ubt-uni.net>
2026-03-26 23:15:00 +0100

    add axios auth interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -16,4 +16,14 @@ api.interceptors.request.use(config => {
   return config
 })
 
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+        }
+      return Promise.reject(error)
+      }
+    )
 export default api
